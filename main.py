39647f9cc6b65d5fb6b14988733b186de11f93cc
Kacper Świderski <kacpers2001@o2.pl>
2024-04-28 15:47:03 +0200

    add products

--- main.py
+++ main.py
@@ -4,13 +4,33 @@ app = Flask(__name__)
 
 # Sample data
 purchases = []
-
+products = []
 # GET endpoint to retrieve all purchases
+
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+# POST endpoint to create a new purchase
+
+
+@app.route('/products', methods=['POST'])
+def create_product():
+
+    new_product = {
+        'id': len(products) + 1,
+        'item': request.args.get('item'),
+        'quantity': int(request.args.get('quantity')),
+        'price': float(request.args.get('price'))
+    }
+    products.append(new_product)
+    return jsonify(new_product), 201
 
 
 @app.route('/purchases', methods=['GET'])
 def get_purchases():
-    return jsonify(purchases)
+    return jsonify(products)
 
 # POST endpoint to create a new purchase
 
@@ -18,6 +38,7 @@ def get_purchases():
 @app.route('/purchases', methods=['POST'])
 def create_purchase():
     new_purchase = {
+        'id': len(purchases) + 1,
         'item': request.args.get('item'),
         'quantity': int(request.args.get('quantity')),
         'price': float(request.args.get('price'))
